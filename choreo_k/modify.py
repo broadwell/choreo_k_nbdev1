925f5f7e296d4906a7a92b5ece3e0aeba09f6cb5
Peter Broadwell <broadwell@stanford.edu>
2021-03-03 15:11:43 -0800

    Semi finalized code

--- choreo_k/modify.py
+++ choreo_k/modify.py
@@ -6,7 +6,8 @@ __all__ = ['get_figure_coords', 'flip_detections', 'zeroify_detections', 'get_bb
            'left_ear_btwn_eye_shoulder', 'right_ear_btwn_eye_shoulder', 'left_elbow_btwn_shoulder_wrist',
            'right_elbow_btwn_shoulder_wrist', 'left_hip_btwn_shoulder_knee_ankle', 'right_hip_btwn_shoulder_knee_ankle',
            'left_ankle_from_knee', 'right_ankle_from_knee', 'correct_pose', 'add_flipped_zeroified_figures',
-           'interpolate_missing_coords', 'TOTAL_COORDS', 'D_THRESH', 'coco_points', 'coco_pts_short']
+           'interpolate_missing_coords', 'output_alphapose_json', 'add_poseflow_figures', 'TOTAL_COORDS', 'D_THRESH',
+           'coco_points', 'coco_pts_short']
 
 # Cell
 from openpifpaf.datasets.constants import COCO_KEYPOINTS, COCO_PERSON_SKELETON
@@ -694,3 +695,72 @@ def interpolate_missing_coords(input_frames, threshold=.5, flip_figures=False, c
         frame_series[i]['zeroified_figures'] = zeroify_detections(frame_series[i]['figures'])
 
     return frame_series
+
+
+def output_alphapose_json(poses_series, figure_type='figures'):
+    ap_data = []
+    image_count = 0
+    for frame in poses_series:
+        image_count += 1
+        image_id = "image" + str(image_count).zfill(5) + '.png'
+        #ap_data[image_id] = []
+        for p, pose in enumerate(frame[figure_type]):
+            score = float(pose.score() * 10)
+            rect_pose = frame[figure_type][p]
+            keypoints = []
+            for kp in pose.data:
+                keypoints.extend([float(kp[0]),float(kp[1]),float(kp[2])])
+            #ap_data[image_id].append({'score': score, 'keypoints': keypoints})
+            ap_data.append({'image_id': image_id, 'score': score, 'keypoints': keypoints})
+
+    with open("alphapose_output.json", 'w') as jfile:
+        json.dump(ap_data, jfile)
+
+#!python2 AlphaPose/PoseFlow/tracker-general.py --in_json alphapose_output.json --out_json alphapose_tracked_output.json --imgdir video_frames/BTS_Fire_Full.mp4/
+
+
+def add_poseflow_figures(input_detections, json_path):
+
+    poses_series = copy.deepcopy(input_detections)
+
+    with open(json_path, 'r') as jfile:
+        pf_data = json.load(jfile)
+    i = 0
+    # NOTE: The image IDs must be sorted properly (not done in original PoseFlow)
+    tracked_poses = 0
+    image_ids = []
+    print("SORTING IMAGE IDS AND GETTING TOTAL TRACKED FIGURES")
+    for image_id in pf_data:
+        image_ids.append(image_id)
+        for figure in pf_data[image_id]:
+            if 'idx' in figure:
+                tracked_poses = max(int(figure['idx'])-1,tracked_poses)
+    print("TOTAL TRACKED POSES",tracked_poses+1)
+    #for image_id in pf_data:
+    for image_id in sorted(image_ids):
+        poses_series[i]['aligned_figures'] = []
+        aligned_figures = {}
+        for figure in pf_data[image_id]:
+            if 'idx' not in figure:
+                continue
+            idx = figure['idx']
+            aligned_figures[int(idx)-1] = figure
+        for p in range(0,tracked_poses):
+            if p in aligned_figures:
+                figure = aligned_figures[p]
+                score = figure['scores']
+                keypoints = figure['keypoints']
+                aligned_keypoints = []
+                print("Adding keypoints for figure number",p,"ID is",figure['idx'])
+                for k in range(0,len(keypoints),3):
+                    aligned_keypoints.append([keypoints[k], keypoints[k+1], keypoints[k+2]])
+            else:
+                aligned_keypoints = []
+            this_annotation = openpifpaf.Annotation(keypoints=COCO_KEYPOINTS, skeleton=COCO_PERSON_SKELETON).set(np.asarray(aligned_keypoints), fixed_score=None)
+            poses_series[i]['aligned_figures'].append(this_annotation)
+            if aligned_keypoints:
+                poses_series[i]['aligned_figures'][poses_series[i]['aligned_figures'].index(this_annotation)].text = str(figure['idx'])
+
+    # Poses are usually already flipped by this point. If not, the code below would work.
+    #if 'flipped_figures' in poses_series[i]:
+    #  poses_series[i]['flipped_figures'][f] = flip_detections(poses_series[i]['aligned_figures'])
